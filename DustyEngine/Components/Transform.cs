6f4a6c023056f0389a999765d8315e1342102c3e
maksym <mini6ins@gmail.com>
2025-03-05 11:04:15 +0000

    add local/global rotation/scale vector to transform

--- DustyEngine/Components/Transform.cs
+++ DustyEngine/Components/Transform.cs
@@ -5,11 +5,25 @@ namespace DustyEngine.Components
     public class Transform : Component
     {
         private Vector3 _localPosition = new Vector3(0, 0, 0);
+        private Vector3 _localRotation = new Vector3(0, 0, 0);
+        private Vector3 _localScale = new Vector3(1, 1, 1);
 
         public Vector3 LocalPosition
         {
             get => _localPosition;
             set { _localPosition = value; }
+        }
+
+        public Vector3 LocalRotation
+        {
+            get => _localRotation;
+            set { _localRotation = value; }
+        }
+
+        public Vector3 LocalScale
+        {
+            get => _localScale;
+            set { _localScale = value; }
         }
 
         [JsonIgnore]
@@ -25,14 +39,44 @@ namespace DustyEngine.Components
             }
         }
 
+        [JsonIgnore]
+        public Vector3 GlobalRotation
+        {
+            get
+            {
+                if (Parent.Parent == null)
+                    return _localRotation;
+
+                var parentTransform = Parent.Parent.GetComponent<Transform>();
+                return parentTransform != null ? parentTransform.GlobalRotation + _localRotation : _localRotation;
+            }
+        }
+
+        [JsonIgnore]
+        public Vector3 GlobalScale
+        {
+            get
+            {
+                if (Parent.Parent == null)
+                    return _localScale;
+
+                var parentTransform = Parent.Parent.GetComponent<Transform>();
+                return parentTransform != null
+                    ? new Vector3(
+                        parentTransform.GlobalScale.X * _localScale.X,
+                        parentTransform.GlobalScale.Y * _localScale.Y,
+                        parentTransform.GlobalScale.Z * _localScale.Z)
+                    : _localScale;
+            }
+        }
 
         public override string ToString()
         {
-            return $"Local: {LocalPosition}, Global: {GlobalPosition}";
+            return $"Local: {LocalPosition}, Rotation: {LocalRotation}, Scale: {LocalScale}, " +
+                   $"Global: {GlobalPosition}, GlobalRotation: {GlobalRotation}, GlobalScale: {GlobalScale}";
         }
     }
 }
-
 
 public class Vector3
 {
@@ -56,6 +100,9 @@ public class Vector3
 
     public static Vector3 operator -(Vector3 a, Vector3 b) =>
         new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+    public static Vector3 operator *(Vector3 a, Vector3 b) =>
+        new Vector3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
 
     public override string ToString() => $"({X}, {Y}, {Z})";
 }
